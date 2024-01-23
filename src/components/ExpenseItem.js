916592b993d9265f8c28ce1b5b8ecd962cd8a977
Samantha S <148943367+PastelBun@users.noreply.github.com>
2024-01-23 17:22:31 +0200

    tl3

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,10 +5,10 @@ function ExpenseItem(props){
 
     return(
         <div className='expense-item'>
-            <div>(props.expenseData.date.toString())</div>
+            <div>{props.expenseData.date.toString()}</div>
             <div className='expense-item__description'>
-                <h2>(props.expenseData.title)</h2>
-                <div className='expense-item__price'>(props.expenseData.price)</div>
+                <h2>{props.expenseData.title}</h2>
+                <div className='expense-item__price'>{props.expenseData.price}</div>
             </div>
         </div>
     )
