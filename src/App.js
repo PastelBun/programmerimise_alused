85d888164ff24fcd1cafa7c627c77abf41914b87
Samantha S <148943367+PastelBun@users.noreply.github.com>
2024-01-23 18:19:49 +0200

    ul2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
-  const expenses=[
-    {
-      date: new Date(2023, 9, 6),
-      title:'New book',
-      price:30.99
-    },
-    {
-      date: new Date(2023, 9, 6),
-      title: 'New jeans',
-      price: 99.99
-    }
-  ]
   return (
     <div className="App">
-      <ExpenseItem
-      expenseData={expenses[0]}
-       />
-       <ExpenseItem
-      expenseData={expenses[1]}
+      <Expenses
+      expenses={expenses}
        />
     </div>
   );
