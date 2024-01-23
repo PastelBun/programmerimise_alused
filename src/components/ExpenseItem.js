c341517dd41c42b4e71569f2c1bdd27f8cc99e30
Samantha S <148943367+PastelBun@users.noreply.github.com>
2024-01-23 16:54:48 +0200

    tl2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,9 +4,9 @@ function ExpenseItem(){
     return(
         <div className='expense-item'>
             <div>Date</div>
-            <div className='expense-item_description'>
+            <div className='expense-item__description'>
                 <h2>Title</h2>
-                <div className='expense-item_price'>Amount</div>
+                <div className='expense-item__price'>Amount</div>
             </div>
         </div>
     )
