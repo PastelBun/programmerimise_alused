a880e0fccac87182d45534078f204d5c84f13919
Sam <djcsdj@hotmail.com>
2024-01-26 15:05:40 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import NewExpense from './components/NewExpense/NewExpense'
 
 
 const App=()=> {
+  const expenses=[
+    {
+      date: new Date(2023, 9, 6),
+      title:'New book',
+      price:30.99
+    },
+    {
+      date: new Date(2023, 9, 6),
+      title: 'New jeans',
+      price: 99.99
+    }
+]
   const addExpenseHandler=(expense)=>{
     console.log('In App.js')
     console.log(expense)
