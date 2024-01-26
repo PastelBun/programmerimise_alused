c9a23fa8c0a2f778a5b7f524e065fd1f2c631f94
Sam <djcsdj@hotmail.com>
2024-01-26 15:07:38 +0200

    test_2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,18 +4,7 @@ import Card from '../UI/Card'
 
 
 const Expenses=()=>{
-    const expenses=[
-        {
-          date: new Date(2023, 9, 6),
-          title:'New book',
-          price:30.99
-        },
-        {
-          date: new Date(2023, 9, 6),
-          title: 'New jeans',
-          price: 99.99
-        }
-    ]
+    
     
     return(
         <Card className='expenses'>
