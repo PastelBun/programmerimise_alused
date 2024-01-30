26615c90d4654ec8e3eaab95df217e7fed83680c
Sam <djcsdj@hotmail.com>
2024-01-30 14:13:18 +0200

    another attempt at connecting expensesfilter and expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 import Card from '../UI/Card'
@@ -7,15 +7,20 @@ import ExpensesFilter from './ExpensesFilter'
 const Expenses=(props)=>{
     /*const expenses=props;*/
     const {expenses}=props;
+    const YearChangeHandler=(event)=>{
+        setEnteredYear(event.target.value)
+    } 
     
     return(
         <Card className='expenses'>
             <ExpensesFilter/>
             <ExpenseItem
             expenseData={expenses[0]}
+            value={expenseData.year} 
             />
             <ExpenseItem
             expenseData={ expenses[1]}
+            value={expenseData.year} 
             />
         </Card>
     );
