4b418eab259900aeebf8f3b5fd8c2d4fa23cd1b7
Sam <djcsdj@hotmail.com>
2024-01-30 15:53:50 +0200

    fix

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
-/*import{ useState } from 'react'*/
+import{ useState } from 'react'
 
 const ExpenseItem=(props)=>{
     const[title/*, setTitle*/]= useState(props.expenseData.title)
