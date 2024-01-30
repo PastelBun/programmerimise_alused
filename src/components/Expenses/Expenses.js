05b798d5e845b944023fde402656ba25afa6ce1b
Sam <djcsdj@hotmail.com>
2024-01-30 13:56:45 +0200

    going back to a version i know and understand

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,29 +1,22 @@
-import React, { useState }  from 'react'
+import React from 'react'
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
 
 const Expenses=(props)=>{
+    /*const expenses=props;*/
     const {expenses}=props;
-    const [filteredYear, setFilteredYear] = useState('2023'); 
-    const filterChangeHandler=(selectedYear)=>{
-        setFilteredYear(selectedYear);
-    };
-
-    const filteredExpenses = expenses.filter(expense => {
-        return expense.date.getFullYear().toString() === filteredYear;
-    });
-
+    
     return(
         <Card className='expenses'>
-            <ExpensesFilter selected={filteredYear} 
-            onChangeFilter={filterChangeHandler} />
-            {filteredExpenses.map((expense) => (
-            <ExpenseItem key={expense.id} 
-            expenseData={expenses}
+            <ExpensesFilter/>
+            <ExpenseItem
+            expenseData={expenses[0]}
             />
-            ))} 
+            <ExpenseItem
+            expenseData={ expenses[1]}
+            />
         </Card>
     );
 }
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,16 +1,13 @@
 import React from 'react';
+
 import './ExpensesFilter.css';
 
-const ExpensesFilter = (props) => {
-    const yearChangeHandler=(event)=>{
-        props.onYearChange(event.target.value);
-    };
-
+const ExpensesFilter = () => {
     return (
         <div className='expenses-filter'>
             <div className='expenses-filter__control'>
                 <label>Filter by year</label>
-                <select value={props.selected} onChange={yearChangeHandler} >
+                <select>
                     <option value='2023'>2023</option>
                     <option value='2024'>2024</option>
                     <option value='2025'>2025</option>
