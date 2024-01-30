c03b1607bf2909ef898e892c6825926eccdd307c
Sam <djcsdj@hotmail.com>
2024-01-30 12:11:36 +0200

    trying to fix expenses;

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,18 @@
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 import Card from '../UI/Card'
-import expenseData from App
-import App from '../../App'
 
 
-const Expenses=()=>{
-    
+const Expenses=(props)=>{
+    const{ expenses } =props;
     
     return(
         <Card className='expenses'>
             <ExpenseItem
-            expenseData={props.App.expenses[0]}
+            expenseData={expenses[0]}
             />
             <ExpenseItem
-            expenseData={props.App.expenses[1]}
+            expenseData={expenses[1]}
             />
         </Card>
     )
