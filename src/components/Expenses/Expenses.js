8a72a830672023e9ba958a0de2f51659d2a905e1
Sam <carlos.soosaar@hotmail.com>
2024-07-25 16:00:01 +0300

    Ül 5 working solution

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,24 +2,22 @@ import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
+import React, { useState } from 'react';
 
 const Expenses=(props)=>{
-    /*const expenses=props;*/
-    const {expenses}=props;
-    /*const YearChangeHandler=(event)=>{
-        setEnteredYear(event.target.value)
-    } */
-    props.expenses.map((expense)=>{
-        console.log(expense)
-    })
-    return(
+    
+    const [filteredExpenses, setFilteredExpenses] = useState(props.expenses);
+
+    const handleFilterChange = (filteredExpenses) => {
+    setFilteredExpenses(filteredExpenses);
+    };
+
+    return(         
         <Card className='expenses'>
-            <ExpensesFilter/>
-            {
-                props.expenses.map((expense)=>{
-                    return <ExpenseItem expenseData={expense} key={expense.id}/>
-                })
-            }
+            <ExpensesFilter expenses={props.expenses} onFilterChange={handleFilterChange} />
+            {filteredExpenses.map((expense) => (
+                <ExpenseItem expenseData={expense} key={expense.id} />
+            ))}
         </Card>
     );
 }
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,28 +1,3 @@
-/*import './ExpensesFilter.css';
-
-const ExpensesFilter = () => {
-    
-    const YearSelectHandler=(event)=>{ 
-        console.log(event.target.value)
-        const Years=App.expense.Year(Date).filter(Selected)
-        Selected=return App.expense===event.target.value
-    }
-    props.App.expense
-    return (
-        <div className='expenses-filter'>
-            <div className='expenses-filter__control'>
-                <label>Filter by year</label>
-                <select onChange={YearSelectHandler}>
-                    <option value='2023'>2023</option>
-                    <option value='2024'>2024</option>
-                    <option value='2025'>2025</option>
-                </select>
-            </div>
-        </div>
-    );
-};
-
-export default ExpensesFilter;*/
 import React, { useState } from 'react';
 import './ExpensesFilter.css';
 
