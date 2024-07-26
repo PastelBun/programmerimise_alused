3c87afb53cbeb594980e4ea6b47e861d38f7e8c3
Sam <carlos.soosaar@hotmail.com>
2024-07-26 14:35:23 +0300

    added locally saved data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const DYMMY_EXPENSES=[
   {
@@ -25,7 +25,14 @@ const DYMMY_EXPENSES=[
 ]
 
 const App=()=> {
-  const [expenses,setExpenses]= useState(DYMMY_EXPENSES)
+  const [expenses,setExpenses]= useState(()=>{
+    const expensesFromLS= JSON.parse(localStorage.getItem('expenses'));
+    return expensesFromLS || [];
+  })
+
+  useEffect(()=>{
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+  },[expenses]);
 
   const addExpenseHandler=(expense)=>{
     console.log('In App.js')
