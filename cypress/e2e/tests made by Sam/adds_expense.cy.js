c097feadef58dd79207fcf5718d5e3d092ec01d8
Samantha <djcsdj@hotmail.com>
2024-11-14 15:30:57 +0200

    setup for cypress testing and first test done

---
+++ cypress/e2e/tests made by Sam/adds_expense.cy.js
@@ -0,0 +1,7 @@
+
+it('opens the website', () => {
+    cy.visit('http://localhost:3000')
+}),
+it("presses add expense button", () => {
+    cy.
+})
---
+++ cypress/e2e/tests made by Sam/open_site.cy.js
@@ -0,0 +1,5 @@
+
+
+it('opens the website', () => {
+    cy.visit('http://localhost:3000')
+})
