069dfb80efae4bd222144b114a86c2cdac2adc4b
Samantha <djcsdj@hotmail.com>
2024-12-28 21:31:40 +0200

    expense add test

--- cypress/e2e/tests made by Sam/adds_expense.cy.js
+++ cypress/e2e/tests made by Sam/adds_expense.cy.js
@@ -2,6 +2,11 @@
 it('opens the website', () => {
     cy.visit('http://localhost:3000')
 }),
+it('fills out the form with expense data', ()=>{
+    cy.get('new-expense__controls> input:first').type('cypress test')
+    cy.get('new-expense__controls> input:second').type('0.99')
+    cy.get('new-expense__controls> input:third').type('12.02.2026')
+}),
 it("presses add expense button", () => {
-    cy.
+    cy.get('new-expense__actions > button').click()
 })
